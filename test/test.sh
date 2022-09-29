75e54d03b3c44ca2a40cafcd860d292d259481b5
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-09-29 19:59:53 +0000

    Tests

---
+++ test/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+[[ -d ${0%/*} ]] && cd "${0%/*}"/../
+
+RUN_ID="$1"
+TESTS=$2
+
+./configure
+
+NCPU=$(getconf _NPROCESSORS_ONLN)
+
+    make -j "$NCPU" enable_documentation=no all logtee
+
+    cd test
+
+    # shellcheck disable=SC2012
+    time LOGTEE_TIMEOUT_MS=590000 make \
+        enable_documentation=no \
+        KVERSION="$(
+            cd /lib/modules
+            ls -1 | tail -1
+        )" \
+        QEMU_CPU="IvyBridge-v2" \
+        DRACUT_NO_XATTR=1 \
+        TEST_RUN_ID="$RUN_ID" \
+        ${TESTS:+TESTS="$TESTS"} \
+        -k V=1 \
+        check
