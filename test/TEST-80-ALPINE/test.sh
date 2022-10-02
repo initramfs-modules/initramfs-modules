fcac94c2f81699fd6688eb41aa10305fb83f33b6
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-10-02 21:59:03 +0000

    Restablize

--- test/TEST-80-ALPINE/test.sh
+++ test/TEST-80-ALPINE/test.sh
@@ -73,7 +73,7 @@ test_setup() {
     fi
 
     dracut -l -i "$TESTDIR"/overlay / \
-        --modules "busybox mdev-alpine rootfs-block test debug watchdog" \
+        --modules "dash mdev-alpine rootfs-block test debug watchdog" \
         --omit "rngd" \
         --drivers "ext4 sd_mod" \
         --install "mkfs.ext4" \
