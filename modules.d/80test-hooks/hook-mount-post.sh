0d76b7579da8382c04ea731266f83298916dcbad
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-10-27 22:05:20 +0000

    Should still fail

--- modules.d/80test-hooks/hook-mount-post.sh
+++ modules.d/80test-hooks/hook-mount-post.sh
@@ -2,13 +2,6 @@
 
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
-die "gombi"
-
 if ismounted "/sysroot"; then
   die "exit"
 fi
-
-# Make sure that /dev/root already exists and mounted
-[ -h /dev/root ] || die "exit"
-
-#echo "dracut-root-block-success" | dd oflag=direct,dsync of=/dev/disk/by-id/ata-disk_marker
--- modules.d/80test-hooks/module-setup.sh
+++ modules.d/80test-hooks/module-setup.sh
@@ -4,6 +4,10 @@ check() {
     return 255
 }
 
+depends() {
+    echo base
+}
+
 install() {
     inst_hook mount 90 "$moddir/hook-mount-post.sh"
 }
