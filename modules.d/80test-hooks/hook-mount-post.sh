36de8bdd2a8c9a46773c165735cb70dc1623a28f
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-10-27 22:11:41 +0000

    Expected to pass

--- modules.d/80test-hooks/hook-mount-post.sh
+++ modules.d/80test-hooks/hook-mount-post.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+
 # Expected to run after all dracut module mount hooks
 
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
-if ismounted "/sysroot"; then
+mount -v
+
+if ! ismounted "/sysroot"; then
   die "exit"
 fi
---
+++ modules.d/80test-hooks/hook-mount-pre.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Expected to run before all dracut module mount hooks
+
+mv /lib/dracut/hooks/mount/99-mount-root.sh /lib/dracut/hooks/mount/79-mount-root.sh
