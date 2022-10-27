a346220d812b67762bd19fedfbd0fa4930204305
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-10-27 21:56:28 +0000

    Make sure it fails firts

--- modules.d/80test-hooks/hook-mount-post.sh
+++ modules.d/80test-hooks/hook-mount-post.sh
@@ -1,5 +1,13 @@
 # Expected to run after all dracut module mount hooks
 
+type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
+
+die "gombi"
+
+if ismounted "/sysroot"; then
+  die "exit"
+fi
+
 # Make sure that /dev/root already exists and mounted
 [ -h /dev/root ] || die "exit"
 
