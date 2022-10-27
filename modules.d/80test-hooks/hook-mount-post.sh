603a8a864a234451985b69a402b15a9760d5b196
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-10-27 21:47:25 +0000

    Make sure it fails

--- modules.d/80test-hooks/hook-mount-post.sh
+++ modules.d/80test-hooks/hook-mount-post.sh
@@ -1,2 +1,6 @@
-#[ -h /dev/root ] && return 1
-return 1
+# Expected to run after all dracut module mount hooks
+
+# Make sure that /dev/root already exists and mounted
+[ -h /dev/root ] || die "exit"
+
+#echo "dracut-root-block-success" | dd oflag=direct,dsync of=/dev/disk/by-id/ata-disk_marker
