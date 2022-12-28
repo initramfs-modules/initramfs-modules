89590c670e0a98d764d952974876a7805ff7e8b9
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-12-28 13:34:44 +0000

    Add a test fat drive

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -18,6 +18,7 @@ test_run() {
     "$testdir"/run-qemu \
         "${disk_args[@]}" \
         -boot order=d \
+        -drive file=fat:rw:"$TESTDIR" \
         -append "root=LABEL=dracut rd.retry=2 console=ttyS0,115200n81 selinux=0 rd.info panic=1 oops=panic softlockup_panic=1 $DEBUGFAIL" \
         -initrd "$TESTDIR"/initramfs.testing
 
