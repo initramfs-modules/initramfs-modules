a5d6e7a4cd24f388b2ab6999af4e1764fb49b71e
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-12-29 16:54:21 +0000

    test

--- container/packages.sh
+++ container/packages.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # kernel, init
-RUN apk add linux-virt losetup
+apk add linux-virt losetup
 
 # dracut
-RUN apk add dracut --update-cache --repository https://dl-cdn.alpinelinux.org/alpine/edge/testing --allow-untrusted
+apk add dracut --update-cache --repository https://dl-cdn.alpinelinux.org/alpine/edge/testing --allow-untrusted
 
 # Uninstall most mkinitfs files. mkinitfs gets pulled in by linux-virt
-RUN rm -rf /sbin/mkinitfs /usr/share/mkinitfs /etc/mkinitfs /boot/initramfs-*
+rm -rf /sbin/mkinitfs /usr/share/mkinitfs /etc/mkinitfs /boot/initramfs-*
 
-RUN ln -sf /boot/vmlinuz-virt /boot/vmlinuz-$(cd /lib/modules; ls -1 | tail -1)
+ln -sf /boot/vmlinuz-virt /boot/vmlinuz-$(cd /lib/modules; ls -1 | tail -1)
 
 # common
-RUN apk add \
+apk add \
     btrfs-progs cryptsetup dash dmraid mdadm sed lvm2 make sudo e2fsprogs parted bzip2 pigz procps kbd busybox git grep binutils
 
 # common - but distro specific name
-RUN apk add partx gpg multipath-tools openssh squashfs-tools qemu-img qemu-system-x86_64 sfdisk ntfs-3g xz
+apk add partx gpg multipath-tools openssh squashfs-tools qemu-img qemu-system-x86_64 sfdisk ntfs-3g xz
 
 # networking
-RUN apk add nfs-utils libnfsidmap dhclient open-iscsi nbd curl
+apk add nfs-utils libnfsidmap dhclient open-iscsi nbd curl
 
 # btrfs - btrfs-progs - btrfs
 # crypt - cryptsetup - crypt
@@ -37,7 +37,6 @@ RUN apk add nfs-utils libnfsidmap dhclient open-iscsi nbd curl
 # additional dependencies to run tests for networking
 # RUN apk add qemu-block-nfs qemu-modules
 
-RUN cp /sbin/poweroff /sbin/shutdown
-RUN cp /usr/bin/dash /bin/dash
-RUN cp /bin/sh /usr/bin/sh
-
+cp /sbin/poweroff /sbin/shutdown
+cp /usr/bin/dash /bin/dash
+cp /bin/sh /usr/bin/sh
