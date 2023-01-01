19a65633d268654ff43d52f700ba5a3aac83da6a
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-01 02:44:10 +0000

    test

--- test/TEST-00-A/test-init.sh
+++ test/TEST-00-A/test-init.sh
@@ -8,10 +8,6 @@ exec > /dev/console 2>&1
 echo "dracut-root-block-success" | dd oflag=direct,dsync of=/dev/disk/by-id/ata-disk_marker
 
 echo "made it to the rootfs!"
-ls -lRa /dev/disk/
-echo "test" > /etc/test
-ls -la /etc/
-cat /etc/test
 echo "Powering down."
 mount -n -o remount,ro /
 poweroff -f
