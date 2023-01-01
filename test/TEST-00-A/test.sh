383b07a841986a13972bda7c385fbba6b0afffd1
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-01 01:45:47 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -21,7 +21,7 @@ test_run() {
     "$testdir"/run-qemu \
         "${disk_args[@]}" \
         -boot order=d \
-        -device ide-hd,drive=bootdrive -drive file="$TESTDIR"/livedir/rootfs.img,index=0,media=disk,format=raw,id=bootdrive \
+        -device ide-hd,drive=bootdrive -drive file="$TESTDIR"/livedir/rootfs.img,index=1,media=disk,format=raw,id=bootdrive \
         -append "rd.live.overlay.overlayfs=1 root=/dev/sdb rd.retry=2 rd.info console=ttyS0,115200n81 selinux=0 panic=1 oops=panic softlockup_panic=1 $DEBUGFAIL" \
         -initrd "$TESTDIR"/initramfs.testing
 
