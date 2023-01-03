4f2eac44016b347117e630eb4b8afa23df9a696b
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-03 01:49:42 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -19,7 +19,8 @@ ls -la /efi/kernel/initrd.img
 
     # squashfs scsi
     dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
-    "$testdir"/run-qemu "${disk_args[@]}" -initrd /efi/kernel/initrd.img \
+    "$testdir"/run-qemu "${disk_args[@]}" \
+    # -initrd /efi/kernel/initrd.img \
         -drive file="$TESTDIR"/livedir/rootfs.squashfs,format=raw,index=0 \
         -drive file=fat:rw:"$TESTDIR",format=vvfat,label=live \
         -cdrom "$TESTDIR"/livedir/rootfs.iso \
