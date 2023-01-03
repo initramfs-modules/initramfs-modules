9e4d666b7e36559c4af6362545292d248395ff89
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-03 02:55:52 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -15,24 +15,14 @@ test_run() {
     declare -i disk_index=3
     qemu_add_drive_args disk_index disk_args "$TESTDIR"/marker.img marker
 
-#${disk_args[@]} -initrd /efi/kernel/initrd.img -drive file=$TESTDIR/livedir/rootfs.squashfs,format=raw,index=0 -drive file=fat:rw:$TESTDIR,format=vvfat,label=live -cdrom $TESTDIR/livedir/rootfs.iso
-
-#VM_CONFIG='-initrd /efi/kernel/initrd.img'
-
     # squashfs scsi
-#    dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
-#    "$testdir"/run-qemu "${disk_args[@]}" "$VM_CONFIG" \
-#        -append "rd.live.overlay.overlayfs=1 root=live:/dev/sda panic=1 oops=panic $DEBUGFAIL"
-#    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
-
-    # squashfs scsi
-    dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
+    dd if=/dev/zero of=$TESTDIR/marker.img bs=1MiB count=1
     "$testdir"/run-qemu "${disk_args[@]}" -initrd /efi/kernel/initrd.img \
-        -drive file="$TESTDIR"/livedir/rootfs.squashfs,format=raw,index=0 \
+        -drive file=$TESTDIR/livedir/rootfs.squashfs,format=raw,index=0 \
         -drive file=fat:rw:"$TESTDIR",format=vvfat,label=live \
-        -cdrom "$TESTDIR"/livedir/rootfs.iso \
+        -cdrom $TESTDIR/livedir/rootfs.iso \
         -append "rd.live.overlay.overlayfs=1 root=live:/dev/sda panic=1 oops=panic $DEBUGFAIL"
-    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
+    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- $TESTDIR/marker.img || return 1
 
     # vfat ide
     dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
