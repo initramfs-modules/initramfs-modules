dd8ea74bc52800c96c8e22ede830bc7b7dffbc30
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-03 02:48:25 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -17,13 +17,11 @@ test_run() {
 
 #${disk_args[@]} -initrd /efi/kernel/initrd.img -drive file=$TESTDIR/livedir/rootfs.squashfs,format=raw,index=0 -drive file=fat:rw:$TESTDIR,format=vvfat,label=live -cdrom $TESTDIR/livedir/rootfs.iso
 
-read -r -d '' VM_CONFIG << EOM
--initrd /efi/kernel/initrd.img
-EOM
+VM_CONFIG='-initrd /efi/kernel/initrd.img'
 
     # squashfs scsi
     dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
-    "$testdir"/run-qemu "$VM_CONFIG" \
+    "$testdir"/run-qemu "${disk_args[@]}" "$VM_CONFIG" \
         -append "rd.live.overlay.overlayfs=1 root=live:/dev/sda panic=1 oops=panic $DEBUGFAIL"
     grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
 
