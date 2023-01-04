bd85954852aed8fb12b11c9e5d46059bcb75b8b5
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-04 14:19:10 +0000

    test

--- container/initramfs.sh
+++ container/initramfs.sh
@@ -8,7 +8,7 @@ pwd
 
 ls -la /_tmp/dracut
 
-cd /
+cd /_tmp/dracut
 mkdir -p /efi /lib /tmp/dracut
 
 apk upgrade
@@ -22,23 +22,19 @@ apk add git curl xz bzip2 alpine-sdk linux-headers >/dev/null
 # Idea: instead of just going with the alpine default busybox, maybe build it from source, only the modules I need, might be able to save about 0.5M
 
 # grab upstream dracut source
-git clone https://github.com/dracutdevs/dracut.git && cd dracut
+#git clone https://github.com/dracutdevs/dracut.git && cd dracut
 
 # pull in a few PRs
 
 # udevadm over of blkid
 curl https://patch-diff.githubusercontent.com/raw/dracutdevs/dracut/pull/2033.patch | git apply
-#curl https://patch-diff.githubusercontent.com/raw/dracutdevs/dracut/pull/2158.patch | git apply
 
 git diff
 
-#todo
-# grab upstream modules only
-rm -rf /usr/lib/dracut/modules.d && mv /dracut/modules.d /usr/lib/dracut/ # && rm -rf /dracut
+# patch dmsquash-live
+cp -a /_tmp/modules.d/90dmsquash-live/* /usr/lib/dracut/modules.d/90dmsquash-live/
 
-# less is more :-), this is an extra layer to make sure systemd is not needed
-rm -rf /usr/lib/dracut/modules.d/*systemd*
-rm -rf /usr/lib/dracut/modules.d/*network*
+cd /
 
 # udev depends on libkmod
 # rebuild libkmod without openssl lib (libkmod will be dependent on musl and libzstd)
