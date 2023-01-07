bff8d8e3270c7bc0ae5a06969c58f21093cf732a
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-07 15:32:13 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -24,19 +24,19 @@ test_run() {
     qemu_add_drive_args disk_index disk_args "$TESTDIR"/marker.img marker
 
     # squashfs scsi
-    test_me "root=live:/dev/sda"
+#    test_me "root=live:/dev/sda"
 
     # vfat ide
-    test_me "root=LABEL=ISO"
+#    test_me "root=LABEL=ISO"
 
     # isofs cdrom
-    test_me "root=LABEL=live rd.live.dir=livedir rd.live.squashimg=rootfs.squashfs"
+#    test_me "root=LABEL=live rd.live.dir=livedir rd.live.squashimg=rootfs.squashfs"
 
     rm -rf  /boot/vmlinuz*
-    dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
+#    dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
     # run-qemu does not support running without -kernel, todo to fix this
-    "$testdir"/run-qemu "${disk_args[@]}" -cdrom "$TESTDIR"/livedir/linux2.iso
-    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
+    "$testdir"/run-qemu -serial mon:stdio  -cdrom "$TESTDIR"/livedir/linux2.iso
+#    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
 
 # todo  -hda rootdisk.img
 # todo - give index for vfat drive
