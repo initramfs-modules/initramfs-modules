d940467046a48e224d10e55bb5e6466ba6831b48
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-07 17:25:42 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -35,7 +35,7 @@ test_run() {
     rm -rf  /boot/vmlinuz*
 #    dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
     # run-qemu does not support running without -kernel, todo to fix this
-    "$testdir"/run-qemu -serial mon:stdio  -cdrom "$TESTDIR"/livedir/linux2.iso
+    "$testdir"/run-qemu -cdrom "$TESTDIR"/livedir/linux2.iso
 #    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
 
 # todo  -hda rootdisk.img
