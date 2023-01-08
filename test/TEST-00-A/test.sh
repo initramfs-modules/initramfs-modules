5e28120aea1d664179696753d06494f30e8c9a33
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-08 15:38:37 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -49,9 +49,9 @@ fi
         -cdrom "$TESTDIR"/livedir/linux.iso \
         -net none \
 #        -boot order=dc \
-        -global driver=cfi.pflash01,property=secure,value=on \
-        -drive if=pflash,format=raw,unit=0,file="${OVMF_CODE}",readonly=on \
-        -drive if=pflash,format=raw,unit=1,file="${OVMF_VARS}" \
+#        -global driver=cfi.pflash01,property=secure,value=on \
+#        -drive if=pflash,format=raw,unit=0,file="${OVMF_CODE}",readonly=on \
+#        -drive if=pflash,format=raw,unit=1,file="${OVMF_VARS}" \
         -bios /usr/share/OVMF/OVMF_CODE.fd
 #    grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
 
