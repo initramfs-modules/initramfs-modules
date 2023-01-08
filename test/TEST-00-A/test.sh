92f50fa3851707aafaa5723859b55b9a38335aee
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-08 02:45:56 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -32,8 +32,11 @@ test_run() {
 
     rm -rf  /boot/vmlinuz*
     dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
-    # run-qemu does not support running without -kernel, todo to fix this
-    "$testdir"/run-qemu "${disk_args[@]}" -cdrom "$TESTDIR"/livedir/linux2.iso -boot order=dc
+    "$testdir"/run-qemu "${disk_args[@]}" \
+        -drive file="$TESTDIR"/livedir/squashfs.img,format=raw,index=0 \
+        -drive file=fat:rw:"$TESTDIR",format=vvfat,label=live \
+        -cdrom "$TESTDIR"/livedir/linux2.iso \
+        -boot order=dc
     grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
 
 # todo  -hda rootdisk.img
