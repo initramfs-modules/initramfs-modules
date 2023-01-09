d1c9e6326c8266a6064514156ed09e204097512b
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-09 14:44:11 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -35,7 +35,9 @@ test_run() {
     # squashfs on nvme drive (no bootloader)
     dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
     "$testdir"/run-qemu "${disk_args[@]}" -initrd /efi/kernel/initrd.img -net none \
-       -drive file="$TESTDIR"/livedir/squashfs.img,format=raw,index=0,if=virtio-scsi \
+    -device virtio-scsi-pci,id=scsi0,num_queues=4 \
+    -device scsi-hd,drive=drive0,bus=scsi0.0,channel=0,scsi-id=0,lun=0 \
+    -drive file="$TESTDIR"/livedir/squashfs.img,format=raw,if=none,id=drive0 \
        -append "$CMD root=/dev/sda"
     grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
 
