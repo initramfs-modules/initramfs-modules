e37b75e3d9c409dbdffe0e44142c97b91a102e0e
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-10 13:49:58 +0000

    test

--- test/TEST-00-A/test.sh
+++ test/TEST-00-A/test.sh
@@ -132,12 +132,18 @@ xorriso -as mkisofs -output "$TESTDIR"/livedir/linux.iso "$TESTDIR"/dracut.*/ini
 find .
 find /boot
 
+#./kernel/vmlinuz
+#./kernel/initrd.img
+
+cat /etc/os-release
+cat /proc/cmdline
+
 # make unified kernel
 objcopy \
 	--add-section .osrel="/etc/os-release" --change-section-vma .osrel=0x20000 \
 	--add-section .cmdline="/proc/cmdline" --change-section-vma .cmdline=0x30000 \
-	--add-section .linux="/boot/vmlinuz*" --change-section-vma .linux=0x40000 \
-	--add-section .initrd="/efi/kernel/initrd.img" --change-section-vma .initrd=0x3000000 \
+	--add-section .linux="./kernel/vmlinuz" --change-section-vma .linux=0x40000 \
+	--add-section .initrd="./kernel/initrd.img" --change-section-vma .initrd=0x3000000 \
 	/usr/lib/gummiboot/linuxx64.efi.stub /boot/alpine.efi
 
 ls -la  /boot/alpine.efi
