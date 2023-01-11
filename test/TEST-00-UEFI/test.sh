02c6a3804621d1094a0c3821a23389196514d72c
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-11 03:27:25 +0000

    test

--- test/TEST-00-UEFI/test.sh
+++ test/TEST-00-UEFI/test.sh
@@ -9,6 +9,7 @@ test_run() {
     declare -a disk_args=()
     declare -i disk_index=3
     qemu_add_drive_args disk_index disk_args "$TESTDIR"/marker.img marker
+    qemu_add_drive_args disk_index disk_args "$TESTDIR"/ESP/LiveOS/squashfs.img root
 
     OVMF_CODE="/usr/share/OVMF/OVMF_CODE.fd"
     rm -rf  /boot/vmlinuz*
@@ -16,7 +17,6 @@ test_run() {
     # ISO UEFI HARDDISK (isohybrid) scsi-hd
     dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
     "$testdir"/run-qemu "${disk_args[@]}" -net none \
-       -drive file="$TESTDIR"/ESP/LiveOS/squashfs.img,format=raw,index=0 \
        -drive file=fat:rw:"$TESTDIR"/ESP,format=vvfat,label=EFI \
        -global driver=cfi.pflash01,property=secure,value=on \
        -drive if=pflash,format=raw,unit=0,file="${OVMF_CODE}",readonly=on
