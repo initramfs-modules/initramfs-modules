c4c2ac8b60481becee05f87788980bce9b569e19
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-11 02:09:45 +0000

    test

--- test/TEST-00-UEFI/test.sh
+++ test/TEST-00-UEFI/test.sh
@@ -17,7 +17,7 @@ test_run() {
    # ISO UEFI HARDDISK (isohybrid) scsi-hd
     dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
     "$testdir"/run-qemu "${disk_args[@]}" -net none \
-       -drive file=fat:rw:"$TESTDIR"/iso,format=vvfat,label=EFI \
+       -drive file=fat:rw:"$TESTDIR"/livedir,format=vvfat,label=EFI \
        -global driver=cfi.pflash01,property=secure,value=on \
        -drive if=pflash,format=raw,unit=0,file="${OVMF_CODE}",readonly=on
     grep -U --binary-files=binary -F -m 1 -q dracut-root-block-success -- "$TESTDIR"/marker.img || return 1
@@ -46,7 +46,7 @@ test_setup() {
     cat /tmp/cmdline
 
     dracut -l -i "$TESTDIR"/overlay / \
-        --modules "rootfs-block test debug watchdog" \
+        --modules "90dmsquash-live test watchdog" \
         --drivers "ext4 sd_mod" \
         --no-hostonly \
         --force "$TESTDIR"/initramfs.testing "$KVERSION" || return 1
