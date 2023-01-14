3e96de0d453b7d9b9cc3ba92a8ff4fb63c52579b
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-14 02:17:20 +0000

    test

--- test/TEST-00-BOOT/test.sh
+++ test/TEST-00-BOOT/test.sh
@@ -23,7 +23,6 @@ test_run() {
     qemu_add_drive_args disk_index disk_args "$TESTDIR"/marker.img marker
 
     OVMF_CODE="/usr/share/OVMF/OVMF_CODE.fd"
-#    rm -rf  /boot/vmlinuz*
 
    # ISO UEFI HARDDISK (isohybrid) scsi-hd
     dd if=/dev/zero of="$TESTDIR"/marker.img bs=1MiB count=1
@@ -109,8 +108,8 @@ mkdir -p /tmp/iso/LiveOS
 cp "$TESTDIR"/livedir/squashfs.img /tmp/iso/LiveOS/squashfs.img
 cd /tmp/iso
 
-#echo "root=live:/dev/disk/by-label/ISO $CMD" > /tmp/cmdline
-echo "root=live:/dev/disk/by-label/EFI rd.live.overlay.overlayfs=1 panic=1 oops=panic rd.debug rd.udev.debug rd.live.debug rd.info console=ttyS0,115200n81 rd.retry=2" > /tmp/cmdline
+echo "root=live:/dev/disk/by-label/ISO $CMD" > /tmp/cmdline
+#echo "root=live:/dev/disk/by-label/EFI rd.live.overlay.overlayfs=1 panic=1 oops=panic rd.debug rd.udev.debug rd.live.debug rd.info console=ttyS0,115200n81 rd.retry=2" > /tmp/cmdline
 cat /tmp/cmdline
 
 # make unified kernel
