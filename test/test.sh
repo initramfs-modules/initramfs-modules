6e07fe1c5009ebd5aeafc1105e48829b9bd62225
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-11-03 19:30:23 -0400

    debug

--- test/test.sh
+++ test/test.sh
@@ -12,16 +12,17 @@ TESTS=$2
 find /usr/lib/dracut/
 
 # shellcheck disable=SC2012
-#time basedir="/usr/lib/dracut/" LOGTEE_TIMEOUT_MS=590000 make \
-#  enable_documentation=no \
-#  KVERSION="$(
-#    cd /lib/modules
-#    ls -1 | tail -1
-#  )" \
-#  QEMU_CPU="IvyBridge-v2" \
-#  DRACUT_NO_XATTR=1 \
-#  TEST_RUN_ID="$RUN_ID" \
-#  ${TESTS:+TESTS="$TESTS"} \
-#  -k V=1 \
-#  check
+time basedir="/usr/lib/dracut/" LOGTEE_TIMEOUT_MS=590000 make \
+time  LOGTEE_TIMEOUT_MS=590000 make \
+  enable_documentation=no \
+  KVERSION="$(
+    cd /lib/modules
+    ls -1 | tail -1
+  )" \
+  QEMU_CPU="IvyBridge-v2" \
+  DRACUT_NO_XATTR=1 \
+  TEST_RUN_ID="$RUN_ID" \
+  ${TESTS:+TESTS="$TESTS"} \
+  -k V=1 \
+  check
 
