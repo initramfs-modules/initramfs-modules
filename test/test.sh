f90f3b8681e6f85663708ef9d5394c047b61ffc9
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-11-03 22:41:02 -0400

    debug

--- test/test.sh
+++ test/test.sh
@@ -9,8 +9,6 @@ cp /usr/bin/dracut /usr/lib/dracut/dracut.sh
 RUN_ID="$1"
 TESTS=$2
 
-find /usr/lib/dracut/
-
 # shellcheck disable=SC2012
 time basedir="/usr/lib/dracut/" LOGTEE_TIMEOUT_MS=590000 make \
   enable_documentation=no \
